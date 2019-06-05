d0bd6e522d974110a11473ed5c9cf445b42520eb
Carlos <Carlos@DESKTOP-3O8KC18>
2019-06-05 13:50:06 +0100

    modificacion en nombre de variable fila

--- src/refactgit/RefactGit.java
+++ src/refactgit/RefactGit.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package refactgit;
 import javax.swing.JOptionPane;
 
@@ -12,25 +8,23 @@ import javax.swing.JOptionPane;
  */
 public class RefactGit {
 
-    /**
-     * @param args the command line arguments
-     */
-    static int nf=0;
+   //modificacion en nombre de variable
+    static int Numero_Fila=0;
     static int nc =0;
     static int columna =0;
     
     public static void main(String[] args) {
-        // TODO code application logic here
+       
         
         columna= Integer.parseInt(JOptionPane.showInputDialog("Desea realizar la matriz: \n1: Matriz de 3 \n2: Matriz de 4 \n3: Matriz de 5"));
         
         
         crear_matriz(columna);
         
-        int matriz[][] = new int[nf][nc];
+        int matriz[][] = new int[Numero_Fila][nc];
         
         // LLenar matriz
-        for (int i = 0; i < nf; i++) {
+        for (int i = 0; i < Numero_Fila; i++) {
             for (int j = 0; j < nc; j++) {
                  matriz[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese valor para la posicion ["+(i+1)+" "+(j+1)+"] de la matriz"));
             }
@@ -40,7 +34,7 @@ public class RefactGit {
         
         String resultado = "";
         
-        for (int i = 0; i < nf; i++) {
+        for (int i = 0; i < Numero_Fila; i++) {
             for (int j = 0; j < nc; j++) {
                 resultado += matriz[i][j];
                 resultado += " ";
@@ -54,9 +48,9 @@ public class RefactGit {
     
     public static void crear_matriz(int numero) {
         
-        if(numero == 1){nf=3; nc =3; }
-        if(numero == 2){nf=4; nc =4; }
-        if(numero == 3){nf=5; nc =5; }
+        if(numero == 1){Numero_Fila=3; nc =3; }
+        if(numero == 2){Numero_Fila=4; nc =4; }
+        if(numero == 3){Numero_Fila=5; nc =5; }
         
     }
     
